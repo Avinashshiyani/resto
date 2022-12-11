5efcf15d039f091957369b659f0b53fabe96ced3
avinash <avinashshiyani760@example.com>
2022-12-11 11:50:26 +0530

    navbar skeleton done

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESTO</title>
+    <link rel="stylesheet" href="./style.css"><!--custom css-->
+    <script src="https://cdn.tailwindcss.com"></script><!--tailwind css-->
+</head>
+<body>
+    <!-- main background  -->
+    <div class="flex justify-center items-center h-screen w-screen bg-red-200">
+        <!-- secondary background -->
+        <div class="flex justify-center bg-white second-background">
+            <!-- thirdary background -->
+            <div class="bg-green-500 w-11/12 third-background">
+                <!-- navbar -->
+                <div class="flex h-16 bg-red-500">
+                    <!-- navbar three components -->
+                    <div class="w-4/12 h-16 bg-orange-500"></div>
+                    <div class="w-4/12 h-16 bg-orange-400"></div>
+                    <div class="w-4/12 h-16 bg-orange-300"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
