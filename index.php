80d373f9243452260e821523d952654524922c42
avinash <avinashshiyani760@example.com>
2022-12-11 15:00:36 +0530

    navbar completely done

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <title>RESTO</title>
     <link rel="stylesheet" href="./components/style.css"><!--custom css-->
     <script src="https://cdn.tailwindcss.com"></script><!--tailwind css-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Quicksand:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- main background  -->
@@ -16,11 +19,34 @@
             <!-- thirdary background -->
             <div class="bg-green-500 w-11/12 third-background">
                 <!-- navbar -->
-                <div class="flex h-16 bg-red-500">
+                <div class="flex h-16 bg-white">
                     <!-- navbar three components -->
-                    <div class="w-4/12 h-16 bg-orange-500"></div>
-                    <div class="w-4/12 h-16 bg-orange-400"></div>
-                    <div class="w-4/12 h-16 bg-orange-300"></div>
+                    <div class="w-6/12 h-16 flex items-center">
+                        <!-- hamburger images -->
+                        <img src="./images/hamburger.jpg"  class="h-10">
+                        <!-- buttons -->
+                        <div class="flex justify-end h-16 w-full items-center text-lg">
+                            <a href="#" class="px-2">Home</a>
+                            <a href="#" class="px-2">About</a>
+                            <a href="#" class="px-2">Special Dishes</a>
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-center text-5xl w-4/12 h-16 pt-4">
+                        <!-- main logo of restro -->
+                        <p class="logo" style="font-family: 'La Belle Aurore', cursive;">Resto</p>
+                    </div>
+                    <div class="flex w-6/12 h-16">
+                        <!-- buttons -->
+                        <div class="flex justify-start h-16 w-full items-center text-lg">
+                            <a href="#" class="px-2">Menu</a>
+                            <a href="#" class="px-2">Team</a>
+                            <a href="#" class="px-2">Reservation</a>
+                        </div>
+                        <!-- search buttons -->
+                        <div class="flex justify-end items-center h-16">
+                            <img src="./images/search.jpg" class="h-10">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
