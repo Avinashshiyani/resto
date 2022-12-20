3174e372f034973ace5b1428cbb3b2cb871aa651
avinash <avinashshiyani760@example.com>
2022-12-20 20:03:55 +0530

    ajjj div ki ijjay gai

--- index.php
+++ index.php
@@ -4,9 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resto</title>
+    <script src="https://cdn.tailwindcss.com"></script><!--tailwind css-->
+    <link rel="stylesheet" href="./components/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-    
+<body class="bg-red-100 p-4">
+    <!-- white background -->
+    <div class="bg-white flex p-2 justify-center">
+        <div class="flex container justify-around  items-center">
+            <!-- navbar -->
+                <div>
+                <i class="fa-sharp fa-solid fa-bars"></i>
+                </div>
+                <div class="space-x-4">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Special Dishes</a>
+                </div>
+                <div class="text-3xl">
+                    <span>Resto</span>
+                </div>
+                <div class="space-x-4">
+                    <a href="#">Menu</a>
+                    <a href="#">Team</a>
+                    <a href="#">Reservation</a>
+                </div>
+                <div><i class="fa-sharp fa-solid fa-magnifying-glass"></i></div>
+            </div>
+        </div>
+        <!-- navbar over -->
+        <div class="bg-green-100 flex p-2 justify-center">
+            <div class="container flex w-full mx-24 justify-center items-center">
+                <!-- intro-text -->
+                <div class="bg-red-200 w-full">
+                    <div class="bg-green-100 text-6xl mt-40">
+                        <h1>We Love <br>Delicious Food!</h1>
+                    </div>
+                    <div class="bg-green-200 text-lg mt-6">
+                        <a href="#" class="bg-red-500 px-8 py-2 rounded-3xl text-white font-bold">Explore Menu</a>
+                        <a href="#" class="px-5 font-bold"><i class="fa-solid fa-play"></i> Play Video</a>
+                    </div>
+                    <div class="bg-green-300 flex text-lg space-x-24 mt-16">
+                        <div>
+                        <i class="fa-solid fa-rocket fa-4x text-slate-700"></i>
+                        <p>Fast Delivery</p>
+                        </div>
+
+                        <div>
+                        <i class="fa-solid fa-leaf fa-4x text-slate-700"></i>
+                        <p>Fresh Food</p>
+                        </div>
+
+                        <div>
+                        <i class="fa-solid fa-comment fa-4x text-slate-700"></i>
+                        <p>24/7 Support</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- image of main -->
+                <div class="main-image bg-no-repeat bg-center bg-cover">hello</div>
+            </div>
+        </div>
 </body>
 </html>
