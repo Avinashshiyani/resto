7ba8b3acfb3cd046449f3d23a4d73a825a2ef7a6
avinash <avinashshiyani760@example.com>
2022-12-22 19:44:36 +0530

    main section compleated and second main-section started

--- index.php
+++ index.php
@@ -34,37 +34,57 @@
             </div>
         </div>
         <!-- navbar over -->
-        <div class="bg-green-100 flex p-2 justify-center">
+        <!-- intro starts -->
+        <div class="bg-white flex p-2 justify-center">
             <div class="container flex w-full mx-24 justify-center items-center">
                 <!-- intro-text -->
-                <div class="bg-red-200 w-full">
-                    <div class="bg-green-100 text-6xl mt-40">
+                <div class="w-full">
+                    <div class=" text-6xl mt-44">
                         <h1>We Love <br>Delicious Food!</h1>
                     </div>
-                    <div class="bg-green-200 text-lg mt-6">
+                    <div class=" text-lg mt-16">
                         <a href="#" class="bg-red-500 px-8 py-2 rounded-3xl text-white font-bold">Explore Menu</a>
                         <a href="#" class="px-5 font-bold"><i class="fa-solid fa-play"></i> Play Video</a>
                     </div>
-                    <div class="bg-green-300 flex text-lg space-x-24 mt-16">
-                        <div>
+                    <div class="flex text-lg  mt-32 justify-center">
+                        <div class="border-r-4 w-2/4 grid justify-center">
                         <i class="fa-solid fa-rocket fa-4x text-slate-700"></i>
                         <p>Fast Delivery</p>
                         </div>
 
-                        <div>
+                        <div class="border-r-4 w-2/4 grid justify-center">
                         <i class="fa-solid fa-leaf fa-4x text-slate-700"></i>
                         <p>Fresh Food</p>
                         </div>
 
-                        <div>
+                        <div class="border-r-4 w-2/4 grid justify-center">
                         <i class="fa-solid fa-comment fa-4x text-slate-700"></i>
                         <p>24/7 Support</p>
                         </div>
                     </div>
                 </div>
                 <!-- image of main -->
-                <div class="main-image bg-no-repeat bg-center bg-cover">hello</div>
+                <div class="main-image bg-no-repeat bg-center bg-cover">
+                <!-- image upper icons -->
+                    <div class="flex items-end justify-end h-full">
+                        <i class="fa-light fa-less-than fa-4x p-4 px-10 bg-white"></i>
+                        <i class="fa-light fa-greater-than fa-4x p-4 px-10 bg-white"></i>
+                    </div>
+                </div>
             </div>
         </div>
+        <!-- intro over -->
+
+        <!-- intro second starts -->
+        <div class="bg-white p-2 flex">
+            <div class="container bg-red-500 mx-24 mt-16 flex">
+                <div class="w-2/4  main-image bg-no-repeat bg-center bg-cover">
+                </div>
+                <div class="w-2/4">
+                    
+                </div>
+            </div>
+        </div>
+        <!-- intro second overs -->
 </body>
 </html>
